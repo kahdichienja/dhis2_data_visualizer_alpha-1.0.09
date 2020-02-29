24f32ab2ab7fa21c4291eea3cf63147ab6b2e573
ago <ago@localhost.localdomain>
2020-02-29 08:43:11 -0500

    first commit

---
+++ analysis/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'analysis'
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('charts/', views.charts, name='charts'),
+]
+
+
