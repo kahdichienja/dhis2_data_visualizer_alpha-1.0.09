0ecc7cd27cbb7019d5d6dc40684f00098dd519b9
ago <ago@localhost.localdomain>
2020-03-01 10:49:04 -0500

    map $ mega search

--- analysis/urls.py
+++ analysis/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('map/', views.mapdata, name= 'mapdata'),
     path('api/endpoint/', views.queryApi, name = 'apidata'),
     path('megasearch/', views.customSearchView, name = 'customsearch'),
+    path('mega/', views.customAjaxSearchView, name = 'mega'),
+    path('mapdataIframe/', views.mapdataIframe, name = 'mapdataIframe'),
 ]
 
 
--- analysis/views.py
+++ analysis/views.py
@@ -16,9 +16,15 @@ def charts(request):
     context = {}
     template_name = 'staff/chat.html'
     return render(request, template_name, context)
-def mapdata(request):
+
+def mapdataIframe(request):
     context = {}
     template_name = 'staff/kymao.html'
+    return render(request, template_name, context)
+
+def mapdata(request):
+    context = {}
+    template_name = 'staff/map.html'
     return render(request, template_name, context)
 
 def dashboard(request):
@@ -150,3 +156,58 @@ def customSearchView(request):
     template_name = 'staff/customsearch.html'
 
     return render(request, template_name, context)
+
+
+def customAjaxSearchView(request):
+    context = {}
+    if request.method == 'GET':
+        indicator = int(request.GET.get('indicatorparam'))
+        pe = request.GET.get('pe')
+        ouid = request.GET.get('ouid')
+
+        indicator_api_url = f'http://41.89.94.105/dsl/api/indicators/'
+        api_url= f'http://41.89.94.105/dsl/api/indicators/{indicator}?pe={pe}&ouid={ouid}&level=2'
+        organisation_unit_api_url = f'http://41.89.94.105/dsl/api/indicators/{indicator}?pe={pe}&ouid=18&level=2'
+
+        headers = {"Accept": "application/json"}
+    
+        response = requests.get(api_url, headers=headers)
+        indicator_api_resposnse = requests.get(indicator_api_url, headers=headers)
+        organisation_unit_api_response = requests.get(organisation_unit_api_url, headers=headers)
+        
+        response_data = response.json() #Full retrieved data
+
+        #Full retrieved indicator data
+        indicator_context = []
+
+        organisation_unit_context = []
+        organisation_unit_data = organisation_unit_api_response.json()
+        context['indicator_context'] = indicator_api_resposnse.json()
+        context['organisation_unit_context'] = organisation_unit_data['result']['dictionary']['orgunits']
+        
+            
+        pprint(context['organisation_unit_context'])
+        # getting the labels
+        county_list = []
+        # getting the data
+        county_data_list = []
+        #
+        results_dict=response_data['result']
+        orgunit_list = results_dict['dictionary']['orgunits']
+        pprint(results_dict)
+
+        context['county_name_list']=[]
+        context['county_name_list_id']=[]
+        context['period_list'],context['ou_list'] ,context['value_list']  = queryApiData(results_dict, int(indicator)) #change query
+
+
+        for orgunit in orgunit_list:
+            context['county_name_list'].append(orgunit['name'])
+            context['county_name_list_id'].append(orgunit['id'])
+    
+    
+    # template_name = 'staff/customsearch.html'
+
+    # return render(request, template_name, context)
+    
+    return JsonResponse(context)
